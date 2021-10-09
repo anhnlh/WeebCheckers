ed0f9f73d937599e589b635615448721882fd19d
Sierra Tran <smt9483@rit.edu>
2021-10-09 19:34:40 -0400

    Created constants with values for the game view
    I created the constants "TITLE" and "VIEW_NAME" and assigned them values
    that will be used when rendering the game view.

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,5 +1,8 @@
 package com.webcheckers.ui;
 
 public class GetGameRoute {
+    // Values used in the view-model map for rendering the game view.
+    static final String TITLE = "title";
+    static final String VIEW_NAME = "game.ftl";
     
 }
