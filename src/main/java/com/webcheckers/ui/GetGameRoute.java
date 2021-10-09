0960e06d43eeec4d18dfc13daf4f830e02d259c8
Sierra Tran <smt9483@rit.edu>
2021-10-09 19:38:46 -0400

    Created a constructor and "tempateEngine" field
    I created a constructor for the route handler and the field for the
    TemplateEngine.

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -1,8 +1,25 @@
 package com.webcheckers.ui;
 
+import java.util.Objects;
+
+import spark.*;
+
 public class GetGameRoute {
     // Values used in the view-model map for rendering the game view.
     static final String TITLE = "title";
     static final String VIEW_NAME = "game.ftl";
-    
+
+    private final TemplateEngine templateEngine;
+
+    /**
+    * The constructor for the {@code GET /game} route handler.
+    *
+    * @param templateEngine
+    *    The {@link TemplateEngine} used for rendering page HTML.
+    */
+    GetGameRoute(final TemplateEngine templateEngine) {
+        Objects.requireNonNull(templateEngine, "templateEngine is required");
+
+        this.templateEngine = templateEngine;
+    }
 }
