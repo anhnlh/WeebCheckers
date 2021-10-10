bf281a9290d24aebac0b6e5e6f9dcce027c8dfc4
Phil Ganem <philipganem@gmail.com>
2021-10-10 18:15:47 -0400

    Model class Row in progress

--- src/main/java/com/webcheckers/model/Row.java
+++ src/main/java/com/webcheckers/model/Row.java
@@ -1,11 +1,42 @@
 package com.webcheckers.model;
-
+import java.util.ArrayList;
 import java.util.Iterator;
 
+/**
+ * Model class for a checker board row
+ * 
+ * @Author Phil Ganem and Sierra Tran
+ */
 public class Row implements Iterable<Space> {
-    private int index;
 
-    public int getIndex() {
+    //Possible colors of a space
+    private enum SPACECOLOR{Black, Red}
+    //Index of a given row
+    private final int index;
+    //Spaces within a given row
+    private final ArrayList<Space> spaces;
+
+    /**
+     * Constructor for a populated row
+     * @param index
+     * @param color
+     * @param blackSpace
+     */
+    public Row(int index, SPACECOLOR color, boolean blackSpace){
+        this.spaces = new ArrayList<Space>();
+        this.index = index;
+    }
+
+    /**
+     * Constructor for an empty row
+     * @param index
+     */
+    public Row(int index){
+        this.spaces = new ArrayList<Space>();
+        this.index = index;
+    }
+
+    public int getIndex(int index) {
         return index;
     }
 
