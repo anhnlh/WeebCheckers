2b2baf2ce50208478c0464b29b05119d989f682b
Sierra Tran <smt9483@rit.edu>
2021-10-09 21:45:40 -0400

    Created "cellIdx" field, started 3 functions
    I created the "cellIdx" field. I also made the 3 stubbed-out functions:
    "getCellIdx", "isValid" and "getPiece".

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -1,5 +1,17 @@
 package com.webcheckers.model;
 
 public class Space {
-    
+    private int cellIdx;
+
+    public int getCellIdx() {
+        return cellIdx;
+    }
+
+    public boolean isValid() {
+        return false;
+    }
+
+    public Piece getPiece() {
+        return null;
+    }
 }
