5fa848fde8060b0654179c26befbc895edb2a3c6
Sierra Tran <smt9483@rit.edu>
2021-10-09 21:40:38 -0400

    Started the "getType" and "getColor" functions
    I created and stubbed-out the functions "getType" and getColor.

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -4,4 +4,11 @@ public class Piece {
     private enum type { SINGLE, KING }
     private enum color { RED, WHITE }
 
+    public type getType() {
+        return null;
+    }
+
+    public color getColor() {
+        return null;
+    }
 }
