80b4fe743d978383c723b9f2b6358ac55b698bb1
Mohammed Alam <ma3655@rit.edu>
2021-10-10 20:55:41 -0400

    Row & BoardView incomplete

--- src/main/java/com/webcheckers/model/BoardView.java
+++ src/main/java/com/webcheckers/model/BoardView.java
@@ -2,10 +2,45 @@ package com.webcheckers.model;
 
 import java.util.Iterator;
 
+/**
+ * Model class for a checker board row
+ * 
+ * @Author Mohammed Alam
+ */
+
 public class BoardView implements Iterable<Row> {
+
+    public enum UserColor {
+        RED, WHITE
+    }
+
+    // The board length
+    static final int BOARD_LENGTH = 8;
+    // The color of the player using the boardView
+    private final UserColor color;
+
+    public BoardView(UserColor color) {
+        this.color = color;
+        board = new Board(color);
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public static int getBoardLength() {
+        return BOARD_LENGTH;
+    }
+
+    public UserColor getColor() {
+        return color;
+    }
 
     @Override
     public Iterator<Row> iterator() {
-        return null;
+        List<Row> rows = new ArrayList<>();
+        for (int i = 0; i < BOARD_LENGTH; i++)
+            rows.add(new Row(i, board));
+        return rows.iterator();
     }
 }
--- src/main/java/com/webcheckers/model/Row.java
+++ src/main/java/com/webcheckers/model/Row.java
@@ -1,37 +1,47 @@
 package com.webcheckers.model;
+
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Model class for a checker board row
  * 
- * @Author Phil Ganem and Sierra Tran
+ * @Author Phil Ganem, Sierra Tran and Mohammed Alam
  */
 public class Row implements Iterable<Space> {
 
-    //Possible colors of a space
-    private enum SPACECOLOR{Black, Red}
-    //Index of a given row
+    // Possible colors of a space
+    private enum SPACECOLOR {
+        Black, Red
+    }
+
+    // Index of a given row
     private final int index;
-    //Spaces within a given row
+    // Spaces within a given row
     private final ArrayList<Space> spaces;
+    // If the row starts with a blank space or not
+    private boolean startsBlackSpace;
 
     /**
      * Constructor for a populated row
+     * 
      * @param index
      * @param color
      * @param blackSpace
      */
-    public Row(int index, SPACECOLOR color, boolean blackSpace){
+    public Row(int index, SPACECOLOR color, boolean blackSpace) {
         this.spaces = new ArrayList<Space>();
         this.index = index;
+        this.startsBlackSpace = blackSpace;
     }
 
     /**
      * Constructor for an empty row
+     * 
      * @param index
      */
-    public Row(int index){
+    public Row(int index) {
         this.spaces = new ArrayList<Space>();
         this.index = index;
     }
@@ -42,6 +52,14 @@ public class Row implements Iterable<Space> {
 
     @Override
     public Iterator<Space> iterator() {
-        return null;
+        boolean isValid = false;
+        for (int i = 0; i < 8; i++) {
+            if ((i % 2 != 0) && (index == 4 || index == 3)) {
+                isValid = true;
+            }
+            if ((i % 2 != 0))
+            spaces.add(new Space(i + (index * 8), isValid,  ));
+        }
+        return spaces.iterator();
     }
 }
