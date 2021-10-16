bba55cb52d434cccc42b311e6208cce901e85ae5
Mohammed Alam <ma3655@rit.edu>
2021-10-16 02:32:30 -0400

    Testing branch and Row test made

---
+++ src/main/java/com/webcheckers/tests/TestRow.java
@@ -0,0 +1,57 @@
+package com.webcheckers.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+@Testable
+public class TestRow {
+
+    @Test
+    public void testGetIndex() {
+        // make a row wiht a new index
+
+        // assert expected index vs result form getIndex()
+    }
+
+    // Used to test get spaces when a new game is called.
+    @Test
+    public void getSpacesNew() {
+        // Our spaces arraymade with getSpace()
+        // new Row() class
+        // ArrayList currentSpaces = Row().getSpaces()
+
+        // Code adding a new Space() using addSpace()
+
+        // Asserting new space is in our list by making sure to
+        // call getSpaces again()
+    }
+
+    // Used to test get spaces when mdiway through a game
+    @Test
+    public void getSpacesInGame() {
+        // Our spaces arraymade with getSpace()
+        // new Row() class
+        // ArrayList currentSpaces = Row().getSpaces()
+
+        // Code adding a new Space() using addSpace()
+
+        // Asserting new space is in our list by making sure to
+        // call getSpaces again()
+    }
+
+    @Test
+    public void testAddSpace() {
+        // Our spaces arraymade with getSpace()
+        // new Row() class
+        // ArrayList currentSpaces = Row().getSpaces()
+
+        // Code adding a new Space() using addSpace()
+
+        // Asserting new space is in our list by making sure to
+        // call getSpaces again()
+    }
+}
