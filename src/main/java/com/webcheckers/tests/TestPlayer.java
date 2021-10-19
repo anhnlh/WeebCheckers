cb4068d8c6a97116fa6002a1cc2b55ea3e8b57dc
RhamzThev <rhamzthev@gmail.com>
2021-10-19 06:02:38 -0400

    created TestPlayer.java

---
+++ src/main/java/com/webcheckers/tests/TestPlayer.java
@@ -0,0 +1,36 @@
+package com.webcheckers.tests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+@Testable
+public class TestPlayer {
+
+
+    @Test
+    public void testGetName() {
+
+    }
+
+    @Test
+    public void testIsPlaying() {
+
+    }
+
+    @Test
+    public void testSetPlaying() {
+
+    }
+
+    @Test
+    public void testEquals() {
+
+    }
+
+    @Test
+    public void testToString() {
+
+    }
+
+}
