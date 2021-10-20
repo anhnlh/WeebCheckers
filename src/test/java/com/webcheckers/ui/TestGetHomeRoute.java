b7033e32f4479a10ad5e98803bdbfc8e1d516ce8
Sierra Tran <smt9483@rit.edu>
2021-10-20 02:02:22 -0400

    Implemented "setup" function
    I implemented a "setup" function that will be called before each test.

--- src/test/java/com/webcheckers/ui/TestGetHomeRoute.java
+++ src/test/java/com/webcheckers/ui/TestGetHomeRoute.java
@@ -1,10 +1,43 @@
 package com.webcheckers.ui;
 
+import static org.mockito.Mockito.*;
+
+import java.util.HashMap;
+
+import com.webcheckers.app.Game;
+import com.webcheckers.app.PlayerLobby;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.platform.commons.annotation.Testable;
+
+import spark.*;
 
 @Tag("UI-tier")
 @Testable
 public class TestGetHomeRoute {
     
+    private GetHomeRoute CuT;
+
+    private Request request;
+    private Response response;
+    private Session session;
+    private TemplateEngine templateEngine;
+    private PlayerLobby playerLobby;
+    private HashMap<String, Game> gameMap;
+
+
+    @BeforeEach
+    public void setup() {
+        request = mock(Request.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        response = mock(Response.class);
+        templateEngine = mock(TemplateEngine.class);
+
+        playerLobby = new PlayerLobby();
+        gameMap = new HashMap<String, Game>();
+
+        CuT = new GetHomeRoute(gameMap, playerLobby, templateEngine);
+    }
 }
