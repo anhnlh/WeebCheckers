48e8507d8bbf9a299385d05dbec77e2f62729356
Sierra Tran <smt9483@rit.edu>
2021-10-20 00:07:41 -0400

    Initial commit for TestGetHomeRoute.java
    This is the start or birth of the TestGetHomeRoute.java file.
    There is no acual code so far.

---
+++ src/test/java/com/webcheckers/ui/TestGetHomeRoute.java
@@ -0,0 +1,10 @@
+package com.webcheckers.ui;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.platform.commons.annotation.Testable;
+
+@Tag("UI-tier")
+@Testable
+public class TestGetHomeRoute {
+    
+}
