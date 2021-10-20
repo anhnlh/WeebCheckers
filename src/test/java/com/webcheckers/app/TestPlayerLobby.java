f2e97fb0cc0655345a2d76c37adb973bfec44ce8
Sierra Tran <smt9483@rit.edu>
2021-10-20 05:04:02 -0400

    Implemented "setup" function for TestPlayerLobby
    I implemented a "setup" function that will be called before each test.

--- src/test/java/com/webcheckers/app/TestPlayerLobby.java
+++ src/test/java/com/webcheckers/app/TestPlayerLobby.java
@@ -1,5 +1,9 @@
 package com.webcheckers.app;
 
+import java.util.HashMap;
+
+import com.webcheckers.model.Player;
+
 import org.junit.jupiter.api.*;
 import org.junit.platform.commons.annotation.Testable;
 
@@ -7,4 +11,23 @@ import org.junit.platform.commons.annotation.Testable;
 @Testable
 public class TestPlayerLobby {
     
+    private PlayerLobby CuT;
+
+    private Player player1;
+    private Player player2;
+    private Player player3;
+
+    private HashMap<String, Player> lobby;
+    private int lobbyNum;
+    
+    @BeforeEach
+    public void setup() {
+        CuT = new PlayerLobby();
+
+        player1 = new Player("player1");
+        player2 = new Player("player2");
+        player3 = new Player("player3");
+    }
+
+
 }
