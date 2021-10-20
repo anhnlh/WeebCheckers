b96faf348095cb5b5ee2e8b20b32b0216555665e
Sierra Tran <smt9483@rit.edu>
2021-10-20 02:39:05 -0400

    Initial commit for TestPlayerLobby.java
    This is the start or birth of the TestPlayerLobby.java file.
    There is no actual code so far.

---
+++ src/test/java/com/webcheckers/app/TestPlayerLobby.java
@@ -0,0 +1,10 @@
+package com.webcheckers.app;
+
+import org.junit.jupiter.api.*;
+import org.junit.platform.commons.annotation.Testable;
+
+@Tag("Application-tier")
+@Testable
+public class TestPlayerLobby {
+    
+}
