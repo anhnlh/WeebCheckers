d47de489d84ed23b27bae0b481ab1b3089c12843
Sierra Tran <smt9483@rit.edu>
2021-10-20 06:16:48 -0400

    "testAddPlayer" made, adjusted "consturctorTest"
    I implemented the test function "testAddPlayer", which tests the
    "addPlayer" function in the PlayerLobby class.
    
    I also modified "constructorTest" to assert that the constructor does
    not return a null PlayerLobby.

--- src/test/java/com/webcheckers/app/TestPlayerLobby.java
+++ src/test/java/com/webcheckers/app/TestPlayerLobby.java
@@ -1,7 +1,7 @@
 package com.webcheckers.app;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
 
@@ -19,23 +19,59 @@ public class TestPlayerLobby {
     private Player player1;
     private Player player2;
     private Player player3;
-
+    private Player player4;
     private HashMap<String, Player> lobby;
+
     private int lobbyNum;
+    private String player1Name;
+    private String player2Name;
+    private String player3Name;
+    private String player4Name;
     
     @BeforeEach
     public void setup() {
         CuT = new PlayerLobby();
 
-        player1 = new Player("player1");
-        player2 = new Player("player2");
-        player3 = new Player("player3");
+        player1Name = "hi";
+        player2Name = "hello";
+        player3Name = "bye";
+        player4Name = "hi";
+
+        player1 = new Player(player1Name);
+        player2 = new Player(player2Name);
+        player3 = new Player(player3Name);
+        player4 = new Player(player4Name);
+
+        lobby = new HashMap<>();
     }
 
     @Test
     public void constructorTest() {
-        new PlayerLobby();
+        assertNotNull(new PlayerLobby());
     }
 
+    @Test
+    public void testAddPlayer() {
+        // 0 players added
+        assertTrue(CuT.getLobby().isEmpty());
 
+        // 1 player added
+        CuT.addPlayer(player1Name);
+        assertEquals(1, CuT.getLobby().size());
+        assertTrue(CuT.getLobby().containsKey(player1Name.toLowerCase()));
+
+        // 2 players added
+        CuT.addPlayer(player2Name);
+        assertEquals(2, CuT.getLobby().size());
+        assertTrue(CuT.getLobby().containsKey(player2Name.toLowerCase()));
+
+        // 3 players added
+        CuT.addPlayer(player3Name);
+        assertEquals(3, CuT.getLobby().size());
+        assertTrue(CuT.getLobby().containsKey(player3Name.toLowerCase()));
+
+        // Attempted to add player 4, but rejected due to duplicate name
+        CuT.addPlayer(player4Name);
+        assertEquals(3, CuT.getLobby().size());
+    }
 }
