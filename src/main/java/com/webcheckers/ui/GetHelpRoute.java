07e526fce6493a9ce7968125ad6812de663aa64e
Sierra Tran <smt9483@rit.edu>
2021-11-07 22:15:58 -0500

    Started constructor and "handle" method

--- src/main/java/com/webcheckers/ui/GetHelpRoute.java
+++ src/main/java/com/webcheckers/ui/GetHelpRoute.java
@@ -1,5 +1,23 @@
 package com.webcheckers.ui;
 
-public class GetHelpRoute {
-    
+import freemarker.log.Logger;
+import spark.*;
+
+public class GetHelpRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetSignInRoute.class.getName());
+
+    // freemarker file
+    public static final String VIEW_NAME = "help.ftl";
+
+    // freemarker variables
+
+
+    public GetHelpRoute() {
+
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        return null;
+    }
 }
