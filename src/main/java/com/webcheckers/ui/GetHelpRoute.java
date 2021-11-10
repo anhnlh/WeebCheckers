f0745e0108e7a496df5678d27ead43596d578d84
Sierra Tran <smt9483@rit.edu>
2021-11-10 18:39:08 -0500

    Fully implemented GetHelpRoute.java
    I wrote all the code necessary for the "handle" function and made a few
    small changes so everything could work.

--- src/main/java/com/webcheckers/ui/GetHelpRoute.java
+++ src/main/java/com/webcheckers/ui/GetHelpRoute.java
@@ -1,6 +1,8 @@
 package com.webcheckers.ui;
 
+import java.util.*;
 import java.util.logging.*;
+
 import spark.*;
 
 public class GetHelpRoute implements Route {
@@ -10,12 +12,15 @@ public class GetHelpRoute implements Route {
     public static final String VIEW_NAME = "help.ftl";
 
     // freemarker variables
+    public static final String TITLE_ATTR = "title";
 
     // parameter initializations
     private final TemplateEngine templateEngine;
 
 
     public GetHelpRoute(TemplateEngine templateEngine) {
+        Objects.requireNonNull(templateEngine, "templateEngine is required");
+
         this.templateEngine = templateEngine;
 
         LOG.config("GetHelpRoute is initialized.");
@@ -25,6 +30,12 @@ public class GetHelpRoute implements Route {
     public Object handle(Request request, Response response) {
         
         LOG.finer("GetHelpRoute is invoked.");
+
+        // start building the View-Model
+        final Map<String, Object> vm = new HashMap<>();
+
+        // display help title
+        vm.put(TITLE_ATTR, "Help");
         
         return templateEngine.render(new ModelAndView(vm, VIEW_NAME));
     }
