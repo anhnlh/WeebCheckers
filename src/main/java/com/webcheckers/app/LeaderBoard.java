c8caf7d9d04fa9198cc9dd189382f1ed4332dc1d
Mohammed Alam <ma3655@rit.edu>
2021-11-14 19:57:55 -0500

    Player win can be incremented

--- src/main/java/com/webcheckers/app/LeaderBoard.java
+++ src/main/java/com/webcheckers/app/LeaderBoard.java
@@ -1,20 +1,20 @@
 package com.webcheckers.app;
 
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LeaderBoard {
-    private int totalPlayers;
     private HashMap<String, Integer> playerWinMap;
     public static int leaderBoardNum = 0;
 
     public LeaderBoard() {
-        this.totalPlayers = 0;
         this.playerWinMap = new HashMap<String, Integer>();
         leaderBoardNum++;
     }
 
     /**
-     * Adds players to leader Board
+     * Adds players to leader Board, or increments Player win if existing.
      *
      * @param playerName str: Player name
      * @return Bool: true on successful add / false on unsuccessful add
@@ -34,40 +34,13 @@ public class LeaderBoard {
         }
 
         // If name doesnt exist in lobby, add and return true
-        if (!lobby.containsKey(playerName.toLowerCase())) {
-            lobby.put(playerName.toLowerCase(), new Player(playerName));
+        if (!playerWinMap.containsKey(playerName.toLowerCase())) {
+            playerWinMap.put(playerName.toLowerCase(), 0);
+            return true;
+        } else {
+            playerWinMap.put(playerName.toLowerCase(), playerWinMap.get(playerName.toLowerCase() + 1));
             return true;
         }
-
-        return false;
     }
 
-    /**
-     * Increment Player Win
-     *
-     * @param playerName str: Player name
-     * @return Bool: true on successful add / false on unsuccessful add
-     */
-    public synchronized boolean addPlayer(String playerName) {
-
-        // return false if name invalid
-        if (playerName.length() >= 26 || playerName.length() <= 0 || playerName.isEmpty()) {
-            return false;
-        }
-
-        // return false if name invalid
-        Pattern invalid = Pattern.compile("[^A-Za-z0-9-_ ]");
-        Matcher find = invalid.matcher(playerName);
-        if (find.find()) {
-            return false;
-        }
-
-        // If name doesnt exist in lobby, add and return true
-        if (!lobby.containsKey(playerName.toLowerCase())) {
-            lobby.put(playerName.toLowerCase(), new Player(playerName));
-            return true;
-        }
-
-        return false;
-    }
 }
